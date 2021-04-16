b70eb8941bac0b038d51bc3b122d96823b0840af
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-04-16 16:16:27 +0530

    Add: created binary tree and add test case

---
+++ src/test/java/com/binarysearchtree/BinarySearchTreeTest.java
@@ -0,0 +1,16 @@
+package com.binarysearchtree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BinarySearchTreeTest {
+    @Test
+    public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        int size = binaryTree.getSize();
+        Assertions.assertEquals(3, size);
+    }
+}
