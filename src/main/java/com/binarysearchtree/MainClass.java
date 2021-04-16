75aafc72c478f3a39c478288734f8d1478c85f4d
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-04-16 16:11:18 +0530

    ADD: welcome note for binary search tree DS

---
+++ src/main/java/com/binarysearchtree/MainClass.java
@@ -0,0 +1,7 @@
+package com.binarysearchtree;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Welcome this is Binary Search Tree DS");
+    }
+}
